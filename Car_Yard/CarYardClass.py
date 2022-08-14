0ea702a304699e0c87a946489353e2d849299c84
ariel <ari465@gmail.com>
2022-08-14 15:39:58 +0300

    fix5

--- Car_Yard/CarYardClass.py
+++ Car_Yard/CarYardClass.py
@@ -4,31 +4,30 @@ from typing import Dict
 
 class CarYard:
 
-    def display_all_cars_by_type(self, type_car) -> None:
-        for item in self.all_car_dict.values():
-            if item.type_car == type_car:
-                item.car_info()
-
-    def __init__(self, all_car_dict: Dict[int, Car] = {}):
+    def __init__(self, all_car_dict: Dict[int, Car]):
         self.all_car_dict: Dict[int, Car] = all_car_dict
+
+    def display_all_cars_by_type(self, type_car: str) -> None:
+        for car in self.all_car_dict.values():
+            if car.type_car == type_car:
+                car.car_info()
 
     def add_car(self, new_car: Car) -> None:
 
-        if new_car.id_plate not in self.all_car_dict:
-            self.all_car_dict[new_car.id_plate] = new_car
-            print(' ')
-            new_car.car_info()
-            print(' ')
-            print('The new Car added !!')
-        else:
-            print('car License plate already in system,try again')
+        if new_car.id_plate in self.all_car_dict.keys():
+            return print('car License plate already in system,try again')
+        self.all_car_dict[new_car.id_plate] = new_car
+        print(' ')
+        new_car.car_info()
+        print(' ')
+        print('The new Car added !!')
 
     def remove_car(self, id_check: int) -> None:
         try:
             del self.all_car_dict[id_check]
-            print('car removed')
+            print('The car has been deleted from the database')
         except KeyError:
-            print('try again invalid id number')
+            print('try again id doesnt exist in the database')
 
     def display_specific_car(self, id_check: int) -> None:
         try:
