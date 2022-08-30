9f4c0e51829eda45825e1d62bdf3a24adc23e3c1
ariel <ari465@gmail.com>
2022-08-30 16:19:48 +0300

    fix_3

--- Decorators/deco_ex.py
+++ Decorators/deco_ex.py
@@ -1,69 +1,65 @@
-from typing import Callable, Any
+from typing import Callable, Type
 
 
-def deco_1_call_func_twice(func: Callable) -> Callable:
-    def inner(*parms, **dict_parms):
+def call_func_twice(func: Callable) -> Callable:
+    def inner(*args, **kwargs):
         print("The purpose of the decorator is to call the function twice")
-        func(*parms, **dict_parms)
-        func(*parms, **dict_parms)
+        func(*args, **kwargs)
+        func(*args, **kwargs)
         print("The call of this func has ended ")
 
     return inner
 
 
-def deco_2_booksList_check(func: Callable) -> Callable:
-    def inner(*parms, **dict_parms):
+def list_length_check(func: Callable) -> Callable:
+    def inner(*args, **kwargs):
         print("The purpose of the decorator is to check if given list in func is not empty")
-        if len(parms[0]) > 0:
+        if args[0]:
             print('good , list not empty ->> colling the func.....')
-            func(*parms, **dict_parms)
-        else:
-            raise ValueError('the length os the list is empty')
+            func(*args, **kwargs)
 
     return inner
 
 
-def deco_3_printer(func: Callable) -> Callable:
-    def inner(*parms, **dict_parms):
+def printer(func: Callable) -> Callable:
+    def inner(*args, **kwargs):
         print("The purpose of the decorator is to print return value of func ")
-        values = func(*parms, **dict_parms)
+        values = func(*args, **kwargs)
         if values:
             print('good , values is not none ->> colling the func')
-            print(func(*parms, **dict_parms))
+            print(values)
 
     return inner
 
 
-def deco_4_factory_type_check(type: Any) -> Callable:
-    def teyp_check(func: Callable):
-        def inner(*parms):
-            if isinstance(*parms, type):
-                return func(*parms)
+def factory_deco(type_var: Type) -> Callable:
+    def type_check(func: Callable):
+        def inner(*args):
+            if isinstance(*args, type_var):
+                return func(*args)
             else:
                 print("Bad Type")
 
         return inner
 
-    return teyp_check
+    return type_check
 
 
 if __name__ == "__main__":
-
     # from website https://teclado.com/30-days-of-python/python-30-day-29-exercise-solutions/
 
-
-    @deco_1_call_func_twice
+    @call_func_twice
     def try_deco_1(a, b):
         print(f'this func try -> {a} + {b} = {a + b}')
 
 
     try_deco_1(4, 5)
 
-    book = [1, 2, 3]
     print('---------------------------------------------------')
+    book = [1, 2, 3]
 
 
-    @deco_2_booksList_check
+    @list_length_check
     def try_deco_2(my_list):
         print(f'this func show list -> {my_list}')
 
@@ -73,21 +69,21 @@ if __name__ == "__main__":
     print('---------------------------------------------------')
 
 
-    @deco_3_printer
-    def try_deco_3(a, b):
-        print(f'returen values of this func is -> {a + b})')
+    @printer
+    def try_deco_3(*args, **kwargs):
+        return args, kwargs
 
 
-    try_deco_3(1, 2)
+    try_deco_3()
 
     print('---------------------------------------------------')
 
 
-# Ex from website https://www.learnpython.org/en/Decorators
+    # Ex from website https://www.learnpython.org/en/Decorators
 
-    @deco_4_factory_type_check(int)
+    @factory_deco(int)
     def try_deco_4(num):
         return num
 
 
-    print(try_deco_4(6))
+    print(try_deco_4(8))
