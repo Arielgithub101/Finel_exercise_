08f44e0f3862f9e7b9ea6e17f9c1fc139de5a980
ariel <ari465@gmail.com>
2022-08-31 18:47:01 +0300

    fix_only_MatrixClass

--- matrix/main.py
+++ matrix/main.py
@@ -2,40 +2,46 @@ import numpy as np
 from matrixclass import Matrix
 from matrixclass2 import MatrixClass2
 
-a = MatrixClass2(3,3)
-b = MatrixClass2(3,3)
-x = [[1,2,3],[4,5,6]]
+#
+# a = MatrixClass2(3,3)
+# b = MatrixClass2(3,3)
+# x = [[1,2,3],[4,5,6]]
+#
+# print(a.print_matrix())
+# print(a.sum_row(2))
+# print(a.sum_col(2))
+# print(a.compers_sum(x))
+#
+# print('-----------------')
+# print(a+b,)
+# print('-----------------')
+# print(a-b,'!here 2')
+# print('-----------------')
+# print(a*5, '!here 3')
+# print('-----------------')
+#
 
-print(a.print_matrix())
-print(a.sum_row(2))
-print(a.sum_col(2))
-print(a.compers_sum(x))
 
-print('-----------------')
-print(a+b,)
-print('-----------------')
-print(a-b,'!here 2')
-print('-----------------')
-print(a*5, '!here 3')
-print('-----------------')
-
-
-
-c = Matrix(3,3)
-d = Matrix(3,3)
-y = [[1,2,3],[4,5,6]]
+c = Matrix(3, 3)
+d = Matrix(3, 3)
+y = [[5, 5, 5],
+     [5, 5, 5],
+     [5, 5, 5]
+     ]
 
 print(c.print_matrix())
 print(c.sum_row(2))
 print(c.sum_col(2))
 print(c.compers_sum(y))
-
-print('-----------------')
-print(a+b)
-print('-----------------')
-print(a-b)
-print('-----------------')
-print(a*5)
-print('-----------------')
+print(c.get_submatrix(0, 1, 0, 2))
+print(c.get_multiplied_row(0,10))
+print(c.get_multiplied_submatrix(0, 1, 0, 2,10))
 
 
+print('-----------------')
+print(c + d)
+print('-----------------')
+print(c - d)
+print('-----------------')
+print(c * 5)
+print('-----------------')
--- matrix/matrixclass.py
+++ matrix/matrixclass.py
@@ -1,24 +1,21 @@
 class Matrix:
     def __init__(self, row: int, col: int):
-        self.matrix = [[1 for j in range(row)] for i in range(col)]
+        self.matrix = [[4 for j in range(row)] for i in range(col)]
 
     def print_matrix(self) -> None:
         for i in range(len(self.matrix)):
             print(self.matrix[i])
 
-    def get_valu_at(self, row: int, col: int) -> int:
+    def get_value_at(self, row: int, col: int) -> int:
         return self.matrix[row][col]
 
     def sum_row(self, row: int) -> int:
-        sum_item_in_row: int = 0
-        for i in self.matrix[row - 1]:
-            sum_item_in_row += i
-        return sum_item_in_row
+        return sum(self.matrix[row])
 
     def sum_col(self, col: int) -> int:
         sum_item_in_col: int = 0
         for row in range(len(self.matrix)):
-            sum_item_in_col += self.matrix[row][col - 1]
+            sum_item_in_col += self.matrix[row][col]
         return sum_item_in_col
 
     def compers_sum(self, matrix_check: list[list]) -> bool:
@@ -31,6 +28,25 @@ class Matrix:
             for colum in range(len(self.matrix[0])):
                 sum2 += self.matrix[row][colum]
         return sum1 == sum2
+
+    def get_submatrix(self, row_start: int, row_end: int, col_start: int, col_end: int) -> list[list[any]]:
+        result: list[list[int]] = []
+        for i in range(row_start, row_end + 1):
+            sub_result: list[int] = []
+            for j in range(col_start, col_end + 1):
+                sub_result.append(self.matrix[i][j])
+            result.append(sub_result)
+        return result
+
+    def get_multiplied_row(self, index_row: int, scaler: int):
+        return list(map(lambda x: x * scaler, self.matrix[index_row]))
+
+    def get_multiplied_submatrix(self,row_start: int, row_end: int, col_start: int, col_end: int,scaler:int)->list[list[int]]:
+        new_mat = self.get_submatrix(row_start, row_end, col_start, col_end)
+        for i in range(len(new_mat)):
+            for j in range(len(new_mat[0])):
+                new_mat[i][j] = new_mat[i][j] * scaler
+        return new_mat
 
     def __getitem__(self, item):
         return self.matrix[item]
